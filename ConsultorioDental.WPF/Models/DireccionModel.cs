b2f6e547a91f69df4e7333af747045173688da23
Tony Puga <tonypuga@hotmail.com>
2025-04-16 16:00:15 -0500

    Add Propiedades

--- ConsultorioDental.WPF/Models/DireccionModel.cs
+++ ConsultorioDental.WPF/Models/DireccionModel.cs
@@ -3,4 +3,8 @@
 public class DireccionModel : EntityModel
 {
     public int IdDireccion { get; set; }
-    }
+    public int IdDepartamento { get; set; }
+    public int IdProvincia { get; set; }
+    public string Direccion { get; set; } = string.Empty;
+    public string Referencia { get; set; } = string.Empty;
+}
